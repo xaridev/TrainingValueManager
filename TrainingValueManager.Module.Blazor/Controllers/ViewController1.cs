f1cf12ced84f96d1735f2b852a0e1015bb89302e
xaridev <xaridev@gmail.com>
2021-12-15 23:11:50 -0500

    Add project files.

---
+++ TrainingValueManager.Module.Blazor/Controllers/ViewController1.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrainingValueManager.Module.Blazor.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class ViewController1 : ViewController
+    {
+        public ViewController1()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            var valueM = ((BlazorApplication)Application).ServiceProvider.GetRequiredService<MyUserCSService>().GetCS("Admin");
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
---
+++ TrainingValueManager.Module.Blazor/MyUserCSService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingValueManager.Module.Blazor
+{
+    public class MyUserCSService
+    {
+        Dictionary<string, string> store;
+        public MyUserCSService()
+        {
+            store = new Dictionary<string, string>();
+        }
+        public void AddCS(string userName, string cs)
+        {
+            if (!store.ContainsKey(userName))
+            {
+                store.Add(userName, cs);
+            }
+        }
+        public string GetCS(string userName)
+        {
+            string cs;
+            store.TryGetValue(userName, out cs);
+            return cs;
+        }
+    }
+}
